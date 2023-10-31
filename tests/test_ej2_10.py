951ff1a51f69268f03922d8d6a3866cd155d7329
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2023-10-31 17:32:50 +0100

    31

--- tests/test_ej2_10.py
+++ tests/test_ej2_10.py
@@ -7,7 +7,7 @@ from src.ej2_10 import pizza
     ("Si" , "1" , "La pizza es vegetariana y lleva tomate , mozzarella y pimiento"),
     ("no" , "2" , "La pizza es no vegetariana y lleva tomate , mozzarella y jamón"),
     ("NO" , "3" , "La pizza es no vegetariana y lleva tomate , mozzarella y salmón"), 
-     ("sí" , "la otra" , "La pizza es vegetariana y lleva tomate , mozzarella y tofu"),
+     ("sí" , "la otra" , "ERROR"),
     ("No" , "1" , "La pizza es no vegetariana y lleva tomate , mozzarella y peperoni")
 ])
 
