2d1e68c3a68e78e6e31d53b66d0e3c64ea0d1279
fbatlos <148891360+fbatlos@users.noreply.github.com>
2023-11-05 19:20:49 +0100

    05

--- src/ej2_2_5.py
+++ src/ej2_2_5.py
@@ -1,2 +1,20 @@
-def calcular_capital_a_obtener(amount , interes):
-    amount*=1+(interes/100)
+def calcular_capital_a_obtener(amount , interes , años):
+    cont=0
+    tot=0
+    for i in range (años):
+        tot+=amount*(1+(interes/100))
+        tot=round(tot , 2)
+        cont=1+i
+        print (f"Tras el {cont}º año obtendras {tot}€")
+
+
+
+def main():
+    amount = int(input("Cantidad a invertir : "))
+    interes = int(input("Interes porcentual anual : "))
+    años = int(input("Cuantos años vas a invertir : "))
+    print(calcular_capital_a_obtener(amount , interes , años ))
+
+
+if __name__ == "__main__":
+    main()
